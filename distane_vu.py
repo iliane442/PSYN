22300b82536df3d602ad773f22202fcfdf207a4d
iliane442 <blanciliane@gmail.com>
2026-02-15 14:02:51 +0100

    oui

--- distane_vu.py
+++ distane_vu.py
@@ -15,7 +15,7 @@ alt_origin = 20.0 #m
 def distance(alt,angle_assiette):
     #calcul de la distance vu par le lidar
     d = np.sqrt((np.sin(np.radians(angle_assiette))*alt**2+alt**2))
-    print(f"Distance au sol vu par le lidar : {d}m, pour un angle de {angle_assiette}° avec le sol et une altitude de {alt}m")
+    #print(f"Distance au sol vu par le lidar : {d}m, pour un angle de {angle_assiette}° avec le sol et une altitude de {alt}m")
     return d
 
-distance(alt_origin,angle_assiette_max)
+#distance(alt_origin,angle_assiette_max)
